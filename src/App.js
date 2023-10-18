af9412916dd1fa80441c8a85a8ce8e5da67405ab
Ian Lee <ianlee0203@gmail.com>
2023-10-17 23:39:11 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
+
+
 
 function App() {
+  const [quote, setQuote] = useState(0);
+const [author, setAuthor] = useState(0);
+const [link, setLink] = useState(0);
+const changeColor = () =>{
+
+  var symbols, color;
+  symbols="0123456789ABCDEF";
+
+
+ color="#";
+  for(var i = 0; i<6;i++){
+   color=color + symbols[Math.floor(Math.random()*16)];
+  
+  }
+  document.getElementById("container").style.background=color;
+  document.getElementById("quote-box").style.color=color;
+  document.getElementById("twitter-logo").style.background=color;
+  document.getElementById("tweet-quote-container").style.background=color;
+  document.getElementById("new-quote").style.background=color;
+}
+
+  
+useEffect(() => {
+    fetch("http://api.quotable.io/random")
+      .then(res => res.json())
+      .then(
+        (quote) => {
+          setQuote(quote.content);  
+          setAuthor(quote.author);
+        }
+      )
+     changeColor();
+  },[]);
+  
+  const whenClick = () =>{
+    fetch("http://api.quotable.io/random")
+    .then(res => res.json())
+    .then(
+      (quote) => {
+        setQuote(quote.content);  
+        setAuthor(quote.author);
+      }
+    )
+    changeColor();
+  }
+  const createLink = () =>{
+    setLink('https://twitter.com/intent/tweet?text='+'"'+quote+ '"' + ' - ' +author)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"
+ id="container"
+    >
+    <div id="quote-box">
+      <div id="text">
+"{quote}"
+      </div>
+      <div id="author"
+     
+      >
+      - {author}
+      </div>
+    
+
+      <a id="tweet-quote" onClick={createLink} href="https://twitter.com/intent/tweet" target="_blank">
+     <div id="tweet-quote-container"> 
+     
+      <img alt="twitter logo" id="twitter-logo"src="https://www.pinclipart.com/picdir/big/578-5789236_logo-twitter-png-blanc-white-twitter-logo-transparent.png"/>
+      
+      </div>
+      </a>
+      <button
+      id="new-quote"
+      onClick={whenClick}
+     >
+      New quote
+      </button>
+      
+    </div>
+   
     </div>
   );
 }
 
 export default App;
+
